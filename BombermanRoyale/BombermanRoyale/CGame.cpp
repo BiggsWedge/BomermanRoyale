40bab15cbacd0078c201c705e102de4ba3287aa6
Shaquille Coffield <SCoffield@fullsail.edu>
2019-08-06 14:33:10 -0400

    Update CGame.cpp

--- BombermanRoyale/BombermanRoyale/CGame.cpp
+++ BombermanRoyale/BombermanRoyale/CGame.cpp
@@ -17,11 +17,11 @@ struct key
 	inline bool released() { return !currState && prevState; }
 };
 
-static std::vector<key> keys(18);
+static std::vector<key> keys(19);
 
 struct KEYS
 {
-	enum { UP, DOWN, LEFT, RIGHT, ZERO, RMB, SPACE, P2UP, P2LEFT, P2DOWN, P2RIGHT, P1UP, P1LEFT, P1DOWN, P1RIGHT, P2BOMB, P1BOMB };
+	enum { UP, DOWN, LEFT, RIGHT, ZERO, RMB, SPACE, P2UP, P2LEFT, P2DOWN, P2RIGHT, P1UP, P1LEFT, P1DOWN, P1RIGHT, P2BOMB, P1BOMB, HELP_MENU };
 };
 
 static std::vector<int> keycodes =
@@ -42,7 +42,8 @@ static std::vector<int> keycodes =
 	'K',
 	'L',
 	VK_NUMPAD6,
-	'O'
+	'O',
+	VK_F1
 };
 
 
@@ -51,11 +52,13 @@ key GameStateButton;
 key SFXButton;
 key P1BOMB;
 key P2BOMB;
+key ControlScreenButton;
 
 bool Player1 = false;
 bool Player2 = false;
 bool P1EXISTS = false;
 bool P2EXISTS = false;
+bool ControlScreenToggle = false;
 float3 P1POS = { -10.0f, 0.0f, 10.0f };
 float3 P2POS = { 10.0f, 0.0f, -5.0f };
 
@@ -107,6 +110,10 @@ void CGame::Run()
 		SFXButton.prevState = SFXButton.currState;
 		SFXButton.currState = GetAsyncKeyState(keycodes[KEYS::SPACE]) & 0x8000;
 
+		ControlScreenButton.prevState = ControlScreenButton.currState;
+
+		ControlScreenButton.currState = GetAsyncKeyState(keycodes[KEYS::HELP_MENU]) & 0x8000;
+
 		if (fullScreenButton.pressed())
 		{
 			FullScreen = !FullScreen;
@@ -147,6 +154,14 @@ void CGame::Run()
 				P2EXISTS = true;
 			}
 
+		}
+
+		if (ControlScreenButton.pressed())
+		{
+			if (ControlScreenToggle == false)
+				ControlScreenToggle = true;
+			else
+				ControlScreenToggle = false;
 		}
 
 		if (G_FAIL(g_pWindow->ProcessWindowEvents()))
@@ -693,6 +708,19 @@ void CGame::Run()
 					if (renderer->iUsedLoadState == curGameState)
 						p_cRendererManager->RenderObject(*objects[i]);
 				}
+
+				if (ControlScreenToggle == true)
+				{
+					if (objects[i]->GetComponent(COMPONENT_TYPE::RENDERER, cRenderer))
+					{
+						renderer = (TRendererComponent*)cRenderer;
+						if (renderer->iUsedLoadState == GameState::CONTROLS_SCREEN)
+							p_cRendererManager->RenderObject(*objects[i]);
+					}
+				}
+
+
+
 			}
 		}
 
@@ -814,7 +842,7 @@ void CGame::Run()
 		{
 			if (p1Ex)
 			{
-				if (abs(p1x - p1EXx) < 4.9f && abs(p1z - p1EXz) < 2.0f)
+				if (abs(p1x - p1EXx) < 4.5f && abs(p1z - p1EXz) < 1.8f)
 				{
 
 					//delete p1;
@@ -822,7 +850,7 @@ void CGame::Run()
 					P1EXISTS = false;
 
 				}
-				else if (abs(p1z - p1EXz) < 4.9f && abs(p1x - p1EXx) < 2.0f)
+				else if (abs(p1z - p1EXz) < 4.5f && abs(p1x - p1EXx) < 1.8f)
 				{
 					//delete p1;
 					p1 = nullptr;
@@ -831,7 +859,7 @@ void CGame::Run()
 			}
 			if (p2Ex)
 			{
-				if (abs(p1x - p2EXx) < 4.9f && abs(p1z - p2EXz) < 2.0f)
+				if (abs(p1x - p2EXx) < 4.5f && abs(p1z - p2EXz) < 1.8f)
 				{
 
 					//delete p1;
@@ -839,7 +867,7 @@ void CGame::Run()
 					P1EXISTS = false;
 
 				}
-				else if (abs(p1z - p2EXz) < 4.9f && abs(p1x - p2EXx) < 2.0f)
+				else if (abs(p1z - p2EXz) < 4.5f && abs(p1x - p2EXx) < 1.8f)
 				{
 					//delete p1;
 					p1 = nullptr;
@@ -852,7 +880,7 @@ void CGame::Run()
 		{
 			if (p1Ex)
 			{
-				if (abs(p2x - p1EXx) < 4.9f && abs(p2z - p1EXz) < 2.0f)
+				if (abs(p2x - p1EXx) < 4.5f && abs(p2z - p1EXz) < 1.8f)
 				{
 
 					//delete p2;
@@ -860,7 +888,7 @@ void CGame::Run()
 					P2EXISTS = false;
 
 				}
-				else if (abs(p2z - p1EXz) < 4.9f && abs(p2x - p1EXx) < 2.0f)
+				else if (abs(p2z - p1EXz) < 4.5f && abs(p2x - p1EXx) < 1.8f)
 				{
 					//delete p2;
 					p2 = nullptr;
@@ -869,7 +897,7 @@ void CGame::Run()
 			}
 			if (p2Ex)
 			{
-				if (abs(p2x - p2EXx) < 4.9f && abs(p2z - p2EXz) < 2.0f)
+				if (abs(p2x - p2EXx) < 4.5f && abs(p2z - p2EXz) < 1.8f)
 				{
 
 					//delete p2;
@@ -877,7 +905,7 @@ void CGame::Run()
 					P2EXISTS = false;
 
 				}
-				else if (abs(p2z - p2EXz) < 4.9f && abs(p2x - p2EXx) < 2.0f)
+				else if (abs(p2z - p2EXz) < 4.5f && abs(p2x - p2EXx) < 1.8f)
 				{
 					//delete p2;
 					p2 = nullptr;
@@ -920,23 +948,32 @@ void CGame::LoadObject()
 {
 	OBJLoadInfo loadInfo;
 
-	//TCollider collider = GetCenter(v_tMeshTemplates[0]);
 
-	////window data(width, height)
-	//g_pWindow->GetClientWidth(width);
-	//g_pWindow->GetClientHeight(height);
+	TCollider collider = GetCenter(v_tMeshTemplates[0]);
 
-	//loadInfo.position = { 0.0f, 0.0f,0.0f };
-	//loadInfo.forwardVec = { 0.0f, 0.0f, -1.0f };
-	//loadInfo.meshID = 0;
-	//loadInfo.usedDiffuse = DIFFUSE_TEXTURES::CRATE;
-	//loadInfo.usedVertex = VERTEX_SHADER::BASIC;
-	//loadInfo.usedPixel = PIXEL_SHADER::BASIC;
-	//loadInfo.usedInput = INPUT_LAYOUT::BASIC;
-	//loadInfo.usedGeo = -1;
-	//loadInfo.floor = false;
-	//loadInfo.LoadState = 3;
-	//loadInfo.scale = DirectX::XMFLOAT3(1.0f / 50.0f, 1.0f / 50.0f, 1.0f / 50.0f);
+	//window data(width, height)
+	g_pWindow->GetClientWidth(width);
+	g_pWindow->GetClientHeight(height);
+
+	loadInfo.position = { 0.0f, 0.0f,0.0f };
+	loadInfo.forwardVec = { 0.0f, 0.0f, -1.0f };
+	loadInfo.meshID = 0;
+	loadInfo.usedDiffuse = DIFFUSE_TEXTURES::HAY_TEX;
+	loadInfo.usedVertex = VERTEX_SHADER::BASIC;
+	loadInfo.usedPixel = PIXEL_SHADER::BASIC;
+	loadInfo.usedInput = INPUT_LAYOUT::BASIC;
+	loadInfo.usedGeo = -1;
+	loadInfo.floor = false;
+	loadInfo.hasCollider = true;
+	collider.center.x = GetCenter(v_tMeshTemplates[0]).center.x + loadInfo.position.x;
+	collider.center.y = GetCenter(v_tMeshTemplates[0]).center.y + loadInfo.position.y;
+	collider.center.z = GetCenter(v_tMeshTemplates[0]).center.z + loadInfo.position.z;
+
+	loadInfo.collider.center = collider.center;
+	loadInfo.collider.extents = collider.extents;
+	loadInfo.LoadState = 3;
+	loadInfo.scale = DirectX::XMFLOAT3(1.0f / 50.0f, 1.0f / 50.0f, 1.0f / 50.0f);
+
 
 	//objects.push_back(p_cEntityManager->CreateOBJFromTemplate(loadInfo));
 
@@ -945,24 +982,27 @@ void CGame::LoadObject()
 	//loadInfo.scale = DirectX::XMFLOAT3(1.0f, 1.0f, 1.0f);
 	//loadInfo.meshID = 1;
 
-	////collider data
-	//collider.center.x = GetCenter(v_tMeshTemplates[0]).center.x + loadInfo.position.x;
-	//collider.center.y = GetCenter(v_tMeshTemplates[0]).center.y + loadInfo.position.y;
-	//collider.center.z = GetCenter(v_tMeshTemplates[0]).center.z + loadInfo.position.z;
 
-	//loadInfo.collider.Center = collider.center;
-	//loadInfo.collider.Extents = collider.extents;
+	//collider data
+	collider = GetCenter(v_tMeshTemplates[1]);
+	collider.center.x = GetCenter(v_tMeshTemplates[1]).center.x + loadInfo.position.x;
+	collider.center.y = GetCenter(v_tMeshTemplates[1]).center.y + loadInfo.position.y;
+	collider.center.z = GetCenter(v_tMeshTemplates[1]).center.z + loadInfo.position.z;
+
+	loadInfo.collider.center = collider.center;
+	loadInfo.collider.extents = collider.extents;
 
 	//objects.push_back(p_cEntityManager->CreateOBJFromTemplate(loadInfo));
 
 	//loadInfo.position = { -12.0f, 0.0f, 0.0f };
 
-	////collider data
-	//collider.center.x = GetCenter(v_tMeshTemplates[0]).center.x + loadInfo.position.x;
-	//collider.center.y = GetCenter(v_tMeshTemplates[0]).center.y + loadInfo.position.y;
-	//collider.center.z = GetCenter(v_tMeshTemplates[0]).center.z + loadInfo.position.z;
 
-	//loadInfo.collider.Center = collider.center;
+	collider.center.x = GetCenter(v_tMeshTemplates[1]).center.x + loadInfo.position.x;
+	collider.center.y = GetCenter(v_tMeshTemplates[1]).center.y + loadInfo.position.y;
+	collider.center.z = GetCenter(v_tMeshTemplates[1]).center.z + loadInfo.position.z;
+
+	loadInfo.collider.center = collider.center;
+	loadInfo.collider.extents = collider.extents;
 
 	//objects.push_back(p_cEntityManager->CreateOBJFromTemplate(loadInfo));
 
@@ -971,7 +1011,7 @@ void CGame::LoadObject()
 	loadInfo.position = { 0.0f, 0.0f, 2.5f };
 	loadInfo.forwardVec = { 0.0f, 0.0f, -1.0f };
 	loadInfo.meshID = 0;
-	loadInfo.usedDiffuse = DIFFUSE_TEXTURES::CRATE;
+	loadInfo.usedDiffuse = DIFFUSE_TEXTURES::HAY_TEX;
 	loadInfo.usedVertex = VERTEX_SHADER::BASIC;
 	loadInfo.usedPixel = PIXEL_SHADER::BASIC;
 	loadInfo.usedInput = INPUT_LAYOUT::BASIC;
@@ -979,10 +1019,9 @@ void CGame::LoadObject()
 	loadInfo.LoadState = 3;
 	loadInfo.floor = false;
 	loadInfo.scale = DirectX::XMFLOAT3(1.0f / 50.0f, 1.0f / 50.0f, 1.0f / 50.0f);
-	
-
-
 	objects.push_back(p_cEntityManager->CreateOBJFromTemplate(loadInfo));
+
+
 
 
 	//loadInfo.position = { -5.0f, 0.0f, 0.0f };
@@ -995,7 +1034,7 @@ void CGame::LoadObject()
 	loadInfo.position = { 0.0f, -0.8f, 20.0f };
 	loadInfo.forwardVec = { 0.0f, 0.95f, -1.0f };
 	loadInfo.usedDiffuse = DIFFUSE_TEXTURES::NAMES_HUD;
-
+	loadInfo.hasCollider = false;
 	loadInfo.scale = DirectX::XMFLOAT3(2.4f, 0.25f, 1.0f);
 	loadInfo.meshID = 2;
 	objects.push_back(p_cEntityManager->CreateOBJFromTemplate(loadInfo));
@@ -1017,6 +1056,23 @@ void CGame::LoadObject()
 	loadInfo.LoadState = 5;
 	objects.push_back(p_cEntityManager->CreateOBJFromTemplate(loadInfo));
 
+
+	loadInfo.position = { 0.0f, 5.0f, -4.5f };
+	loadInfo.forwardVec = { 0.0f, 0.95f, -1.0f };
+	loadInfo.usedDiffuse = DIFFUSE_TEXTURES::HELP_MENU;
+	loadInfo.scale = DirectX::XMFLOAT3(0.8f, 1.0f, 1.0f);
+	loadInfo.meshID = 2;
+	loadInfo.LoadState = 6;
+	objects.push_back(p_cEntityManager->CreateOBJFromTemplate(loadInfo));
+
+	//loadInfo.position = { 0.0f, -2.4f, 2.93f };
+	//loadInfo.forwardVec = { 0.0f, 0.95f, -1.0f };
+	//loadInfo.usedDiffuse = DIFFUSE_TEXTURES::ARCADE_BUTTON;
+	//loadInfo.scale = DirectX::XMFLOAT3(1.75f, 1.99f, 1.0f);
+	//loadInfo.meshID = 3;
+	//loadInfo.LoadState = 0;
+	//objects.push_back(p_cEntityManager->CreateOBJFromTemplate(loadInfo));
+
 	for (float x = -12.5; x <= 12.5; x += 2.5f)
 	{
 		for (float z = -7.5; z <= 12.5; z += 2.5f)
@@ -1027,8 +1083,35 @@ void CGame::LoadObject()
 			loadInfo.meshID = 0;
 			loadInfo.LoadState = 3;
 			loadInfo.floor = true;
+			loadInfo.hasCollider = true;
+			collider.center.x = GetCenter(v_tMeshTemplates[0]).center.x + loadInfo.position.x;
+			collider.center.y = GetCenter(v_tMeshTemplates[0]).center.y + loadInfo.position.y;
+			collider.center.z = GetCenter(v_tMeshTemplates[0]).center.z + loadInfo.position.z;
+
+			loadInfo.collider.center = collider.center;
+			loadInfo.collider.extents = collider.extents;
+
 			loadInfo.scale = DirectX::XMFLOAT3(1.0f / 40.0f, 1.0f / 40.0f, 1.0f / 40.0f);
 			objects.push_back(p_cEntityManager->CreateOBJFromTemplate(loadInfo));
+		}
+
+	}
+	for (float x = -7.5; x <= 7.5; x += 2.5f)
+	{
+		for (float z = -2.5; z <= 7.5; z += 2.5f)
+		{
+	loadInfo.position = { x, 0.0f, z };
+	loadInfo.forwardVec = { 0.0f, 0.0f, -1.0f };
+	loadInfo.meshID = 0;
+	loadInfo.usedDiffuse = DIFFUSE_TEXTURES::HAY_TEX;
+	loadInfo.usedVertex = VERTEX_SHADER::BASIC;
+	loadInfo.usedPixel = PIXEL_SHADER::BASIC;
+	loadInfo.usedInput = INPUT_LAYOUT::BASIC;
+	loadInfo.usedGeo = -1;
+	loadInfo.LoadState = 3;
+	loadInfo.floor = false;
+	loadInfo.scale = DirectX::XMFLOAT3(1.0f / 50.0f, 1.0f / 50.0f, 1.0f / 50.0f);
+	objects.push_back(p_cEntityManager->CreateOBJFromTemplate(loadInfo));
 		}
 
 	}
@@ -1294,89 +1377,89 @@ void CGame::WindowResize()
 	}
 }
 
-void InitSortedParticles(sorted_pool_t<particle, 1000>& sortedPool, float deltaTime) {
-	//Init number of particles as loop
-	for (size_t i = 0; i < 1; i++) {
-		int count = sortedPool.alloc();
-
-		if (count != -1) {
-			sortedPool[count].pos.x = 0;
-			sortedPool[count].pos.y = 0;
-			sortedPool[count].pos.z = 0;
-			sortedPool[count].speed.x = (-3.0f + (3.0f - -3.0f) * ((float)rand() / (float)RAND_MAX)) / 3;
-			sortedPool[count].speed.y = (0.0f + (2.0f - 0.0f) * ((float)rand() / (float)RAND_MAX)) / 2;
-			sortedPool[count].speed.z = (-3.0f + (3.0f - -3.0f) * ((float)rand() / (float)RAND_MAX)) / 3;
-			sortedPool[count].speed.y *= particleSpeed;
-			sortedPool[count].timer = 1.5f + (1.8f - 1.5f) * ((float)rand() / (float)RAND_MAX);
-			sortedPool[count].color = { 1,0,0,1 };
-		}
-	}
-
-	for (size_t i = 0; i < sortedPool.size(); i++) {
-		sortedPool[i].prev_pos = sortedPool[i].pos;
-		sortedPool[i].pos.x += (sortedPool[i].speed.x * deltaTime);
-		sortedPool[i].pos.y += (sortedPool[i].speed.y * deltaTime);
-		sortedPool[i].pos.z += (sortedPool[i].speed.z * deltaTime);
-		sortedPool[i].speed.y -= particleGravity * deltaTime;
-		sortedPool[i].timer -= deltaTime;
-		add_line(sortedPool[i].pos, sortedPool[i].prev_pos, sortedPool[i].color);
-
-		if (sortedPool[i].timer <= 0) {
-			sortedPool.free(i);
-			i--;
-		}
-	}
-}
-
-void InitFreeParticles(emitter& emitter, pool_t<particle, 1024>& freePool, float deltaTime) {
-	//init emitters
-	firstEmit.spawn_pos = { 5,0,5 };
-	secondEmit.spawn_pos = { 0,0,10 };
-	thirdEmit.spawn_pos = { 5,0,0 };
-	firstEmit.spawn_color = { 0,1,0,0 };
-	secondEmit.spawn_color = { 0,0,1,0 };
-	thirdEmit.spawn_color = { 0,1,1,0 };
-
-	//alloc space
-	int count = freePool.alloc();
-	int emitCount = emitter.indices.alloc();
-
-	emitter.indices[emitCount] = count;
-	freePool[count].pos = emitter.spawn_pos;
-	freePool[count].prev_pos = emitter.spawn_pos;
-	freePool[count].speed.x = (-5.0f + (5.0f - -5.0f) * ((float)rand() / (float)RAND_MAX)) / 3;
-	freePool[count].speed.y = (0.0f + (2.0f - 0.0f) * ((float)rand() / (float)RAND_MAX)) / 2;
-	freePool[count].speed.z = (-3.0f + (3.0f - -3.0f) * ((float)rand() / (float)RAND_MAX)) / 3;
-	freePool[count].speed.x *= particleSpeed;
-	freePool[count].timer = ((float)rand() / (float)RAND_MAX);
-	freePool[count].color = { 0,1,1,0 };
-
-	for (size_t i = 0; i < emitter.indices.size(); i++) {
-		int Ecount = emitter.indices[i];
-
-		if (Ecount <= -1 || Ecount > 1024) {
-			break;
-		}
-
-		if (Ecount != -1) {
-			freePool[Ecount].timer -= deltaTime;
-
-			if (freePool[Ecount].timer <= 0.0f) {
-				freePool.free(Ecount);
-				emitter.indices.free(i);
-				i--;
-				continue;
-			}
-
-			freePool[Ecount].prev_pos = freePool[Ecount].pos;
-			freePool[Ecount].pos.x += (freePool[Ecount].speed.x * deltaTime);
-			freePool[Ecount].pos.y += (freePool[Ecount].speed.y * deltaTime);
-			freePool[Ecount].pos.z += (freePool[Ecount].speed.z * deltaTime);
-			freePool[Ecount].speed.y -= particleGravity * deltaTime;
-			add_line(freePool[Ecount].pos, freePool[Ecount].prev_pos, freePool[Ecount].color);
-		}
-	}
-}
+//void InitSortedParticles(sorted_pool_t<particle, 1000>& sortedPool, float deltaTime) {
+//	//Init number of particles as loop
+//	for (size_t i = 0; i < 1; i++) {
+//		int count = sortedPool.alloc();
+//
+//		if (count != -1) {
+//			sortedPool[count].pos.x = 0;
+//			sortedPool[count].pos.y = 0;
+//			sortedPool[count].pos.z = 0;
+//			sortedPool[count].speed.x = (-3.0f + (3.0f - -3.0f) * ((float)rand() / (float)RAND_MAX)) / 3;
+//			sortedPool[count].speed.y = (0.0f + (2.0f - 0.0f) * ((float)rand() / (float)RAND_MAX)) / 2;
+//			sortedPool[count].speed.z = (-3.0f + (3.0f - -3.0f) * ((float)rand() / (float)RAND_MAX)) / 3;
+//			sortedPool[count].speed.y *= particleSpeed;
+//			sortedPool[count].timer = 1.5f + (1.8f - 1.5f) * ((float)rand() / (float)RAND_MAX);
+//			sortedPool[count].color = { 1,0,0,1 };
+//		}
+//	}
+//
+//	for (size_t i = 0; i < sortedPool.size(); i++) {
+//		sortedPool[i].prev_pos = sortedPool[i].pos;
+//		sortedPool[i].pos.x += (sortedPool[i].speed.x * deltaTime);
+//		sortedPool[i].pos.y += (sortedPool[i].speed.y * deltaTime);
+//		sortedPool[i].pos.z += (sortedPool[i].speed.z * deltaTime);
+//		sortedPool[i].speed.y -= particleGravity * deltaTime;
+//		sortedPool[i].timer -= deltaTime;
+//		add_line(sortedPool[i].pos, sortedPool[i].prev_pos, sortedPool[i].color);
+//
+//		if (sortedPool[i].timer <= 0) {
+//			sortedPool.free(i);
+//			i--;
+//		}
+//	}
+//}
+//
+//void InitFreeParticles(emitter& emitter, pool_t<particle, 1024>& freePool, float deltaTime) {
+//	//init emitters
+//	firstEmit.spawn_pos = { 5,0,5 };
+//	secondEmit.spawn_pos = { 0,0,10 };
+//	thirdEmit.spawn_pos = { 5,0,0 };
+//	firstEmit.spawn_color = { 0,1,0,0 };
+//	secondEmit.spawn_color = { 0,0,1,0 };
+//	thirdEmit.spawn_color = { 0,1,1,0 };
+//
+//	//alloc space
+//	int count = freePool.alloc();
+//	int emitCount = emitter.indices.alloc();
+//
+//	emitter.indices[emitCount] = count;
+//	freePool[count].pos = emitter.spawn_pos;
+//	freePool[count].prev_pos = emitter.spawn_pos;
+//	freePool[count].speed.x = (-5.0f + (5.0f - -5.0f) * ((float)rand() / (float)RAND_MAX)) / 3;
+//	freePool[count].speed.y = (0.0f + (2.0f - 0.0f) * ((float)rand() / (float)RAND_MAX)) / 2;
+//	freePool[count].speed.z = (-3.0f + (3.0f - -3.0f) * ((float)rand() / (float)RAND_MAX)) / 3;
+//	freePool[count].speed.x *= particleSpeed;
+//	freePool[count].timer = ((float)rand() / (float)RAND_MAX);
+//	freePool[count].color = { 0,1,1,0 };
+//
+//	for (size_t i = 0; i < emitter.indices.size(); i++) {
+//		int Ecount = emitter.indices[i];
+//
+//		if (Ecount <= -1 || Ecount > 1024) {
+//			break;
+//		}
+//
+//		if (Ecount != -1) {
+//			freePool[Ecount].timer -= deltaTime;
+//
+//			if (freePool[Ecount].timer <= 0.0f) {
+//				freePool.free(Ecount);
+//				emitter.indices.free(i);
+//				i--;
+//				continue;
+//			}
+//
+//			freePool[Ecount].prev_pos = freePool[Ecount].pos;
+//			freePool[Ecount].pos.x += (freePool[Ecount].speed.x * deltaTime);
+//			freePool[Ecount].pos.y += (freePool[Ecount].speed.y * deltaTime);
+//			freePool[Ecount].pos.z += (freePool[Ecount].speed.z * deltaTime);
+//			freePool[Ecount].speed.y -= particleGravity * deltaTime;
+//			add_line(freePool[Ecount].pos, freePool[Ecount].prev_pos, freePool[Ecount].color);
+//		}
+//	}
+//}
 
 void CGame::GamePlayLoop()
 {
